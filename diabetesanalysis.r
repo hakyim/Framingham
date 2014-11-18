13cb370f3db28192c259f30711ca5bde7e8b64ce
albertkuo <albertkuo@uchicago.edu>
2014-11-18 16:01:54 -0600

    Added comments

--- diabetesanalysis.r
+++ diabetesanalysis.r
@@ -1,68 +1,92 @@
-#Consent Group 2
-data_age<-read.delim("phs000007.v24.pht003099.v3.p9.c2.vr_dates_2011_m_0689s.HMB-IRB-NPU-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
-data_orig<-read.delim("phs000007.v24.pht000040.v4.p9.c2.dbt0_27s.HMB-IRB-NPU-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
-data_offs<-read.delim("phs000007.v24.pht000041.v6.p9.c2.dbt1_7s.HMB-IRB-NPU-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
+## Exploratory Analysis on Diabetes Phenotypes
+
+#--Read In Files--#
 #Consent Group 1
 data_age<-read.delim("phs000007.v23.pht003099.v2.p8.c1.vr_dates_2011_m_0689s.HMB-IRB-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
 data_orig<-read.delim("phs000007.v23.pht000040.v4.p8.c1.dbt0_27s.HMB-IRB-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
 data_offs<-read.delim("phs000007.v23.pht000041.v5.p8.c1.dbt1_7s.HMB-IRB-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
 
-data = merge(data_orig, data_offs, all=T)
-data2 = merge(data, data_age, by="dbGaP_Subject_ID", all.x=T)  ## erases ages!!
-n <- dim(data)[1]*(dim(data)[2]-3) - sum(is.na(data)) #3 columns are subject ID, etc.
+#Consent Group 2
+data_age<-read.delim("phs000007.v24.pht003099.v3.p9.c2.vr_dates_2011_m_0689s.HMB-IRB-NPU-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
+data_orig<-read.delim("phs000007.v24.pht000040.v4.p9.c2.dbt0_27s.HMB-IRB-NPU-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
+data_offs<-read.delim("phs000007.v24.pht000041.v6.p9.c2.dbt1_7s.HMB-IRB-NPU-MDS.txt.gz", header=T, as.is=T, skip=10, sep='\t')
+
+#--Organize Data--#
+
+#Merge Data
+data = merge(data_orig, data_offs, all=T)                      #Original Cohort and Offspring Cohort
+data2 = merge(data, data_age, by="dbGaP_Subject_ID", all.x=T)  #Original Cohort, Offspring Cohort, and their Ages/Dates
+
+#Pre-allocate space for reorganized data (data_result)
+n <- dim(data)[1]*(dim(data)[2]-3) - sum(is.na(data))          #n = number of rows we will have in data_result
 data_result<-data.frame(Values = numeric(n), Variables = numeric(n), ID = numeric(n), Ages = numeric(n), Dates = numeric(n))
 
 k<-1
 for(i in 1:dim(data)[1]){
-loglist<- !is.na(data[i,]) #how to exclude first two and last?
-test<-t(data[i,loglist])
-ID<-test[1,1]
-variables <- colnames(data)[loglist]
-test = cbind(test, variables, rep(ID,each=nrow(test)))
-test = test[4:nrow(test)-1,]
-s <- unlist(strsplit(variables,"[^[:digit:]]"))
-print(i)
-s_cleared <- as.numeric(s[s!=""])
-s_cleared <- s_cleared[s_cleared<29]              #gets rid of problem with dm150, etc.
-ages <- t(data2[i, 2*s_cleared+dim(data)[2]+1]) #+3 with consent group 2
-dates <- t(data2[i, s_cleared+dim(data)[2]+56]) #Date: missing date1, +57 with consent group 2
-if(dim(ages)[1]==1) {				     #Solves problem with binding rownum=1
-	test = c(test,ages,dates)
-	rownum <- 1
-} else {
-test = cbind(test,ages,dates)                 #note: cbind coerces values to character
-rownum <- nrow(test)
-}
-data_result[k:(k+rownum-1),] = test
-k=k+rownum
+  #Get Non-Missing Values and Variable Names for Participant i
+  loglist<- !is.na(data[i,])                                   #loglist = list of column number with values (??how to exclude first two columns and last column)
+  values<-t(data[i,loglist])                                   #values = list of non-missing values for Participant i  
+  ID<-values[1,1]                                              #ID = Subject ID of Participant i
+  variables <- colnames(data)[loglist]                         #variables = variables names of the values
+  values = cbind(values, variables, rep(ID,each=nrow(values))) 
+  values = values[4:nrow(values)-1,]                           #excludes values for ID
+  
+  #Get Ages and Dates for Participant i
+  s <- unlist(strsplit(variables,"[^[:digit:]]"))              #Extract numbers from variable names                                                  
+  s_cleared <- as.numeric(s[s!=""])                            
+  s_cleared <- s_cleared[s_cleared<29]                         #Exams number are from 0-28, e.g. excludes 150 from dm150_curr4 
+  ages <- t(data2[i, 2*s_cleared+dim(data)[2]+1])              #If consent group 2, use +3 instead of +1
+  dates <- t(data2[i, s_cleared+dim(data)[2]+56])              #If consent group 2, use +57 (or 58?) instead of +56 
+                                                               #Bug: no column for date1, will return value for last age column, which should be NA
+  #Bind Values, Ages, and Dates
+  if(dim(ages)[1]==1) {				                                 #Fixes bug when there is only one row
+	  values = c(values,ages,dates)
+	  rownum <- 1
+  } else {
+    values = cbind(values,ages,dates)                          #Note: cbind coerces values to character (??better way to do this)
+    rownum <- nrow(values)
+  }
+  data_result[k:(k+rownum-1),] = values                        #Replace the next empty rows in data_result with values
+  k=k+rownum                                                   #Jump to position of next empty row
+  print(i) 
 }
 
-#convert back to numeric
-data_result$Values <- as.numeric(data_result$Values)
+#Convert values back to numeric
+data_result$Values <- as.numeric(data_result$Values)           
 data_result$ID <- as.numeric(data_result$ID)
 data_result$Ages <- as.numeric(data_result$Ages)
 data_result$Dates <- as.numeric(data_result$Dates)
 
-#separate data
-##original
-data_bls <- data_result[grep("BLS",data_result$Variables),]
-data_dm <- data_result[grep("dm150_curr", data_result$Variables),]
-##offspring
-data_fgluc <- data_result[grep("F_GLUC", data_result$Variables),]
-data_dmrx <- data_result[grep("DMRX", data_result$Variables),]
-data_diab <- data_result[grep("DIAB", data_result$Variables),]
+#Separate organized data into different subsets
+data_bls <- data_result[grep("BLS",data_result$Variables),]           #data_bls = Blood Glucose Level
+data_dm <- data_result[grep("dm150_curr", data_result$Variables),]    #data_dm = Diabetes Status (150 mg/dl)
 
-#counting
-dim(unique(subset(data_diab, Values==1)[,c(1,3)]))
-dim(subset(data_diab[grep("DIAB1", data_diab$Variables),],Values==1))
+##Offspring Cohort
+data_fgluc <- data_result[grep("F_GLUC", data_result$Variables),]     #data_fgluc = Fasting Glucose Level
+data_dmrx <- data_result[grep("DMRX", data_result$Variables),]        #data_dmrx = Treatment for Diabetes Status
+data_diab <- data_result[grep("DIAB", data_result$Variables),]        #data_diab = Diabetes Status
 
-#calculating change in fasting glucose when become diabetic (x is diff)
-df<-data.frame(Ages=c(),Diff=c())
-data_temp<-merge(data_diab,data_fgluc2) #data_fluc2 is data_gluc with renamed columns
+
+#--Analysis of Data--#
+#Count number of participants (Work in Progress)
+dim(unique(subset(data_diab, Values==1)[,c(1,3)]))                    #(?? Returns number of participants who are diabetic)
+dim(subset(data_diab[grep("DIAB1", data_diab$Variables),],Values==1)) #(?? Returns number of participants who are initially diabetic)
+
+#Compute and plot changes in glucose when participant becomes diabetic
+data_diff<-data.frame(Ages=c(),Diff=c())                              #(?? Pre-allocate with number of participants who become diabetic)
+data_fgluc2<-data_fgluc
+#colnames(data_fgluc2)<-c("")
+data_temp<-merge(data_diab,data_fgluc2)                               #data_fluc2 is data_gluc with renamed columns
 for(i in 1:dim(data_temp)[1]){
-if(data_temp[i, 4]==1 && data_temp[i-1,4]==0 && data_temp[i,1]==data_temp[i-1,1]){
-x<-data_temp[i,6] - data_temp[i-1,6]
-age <- data_temp[i, 2]
-df<-rbind(df,cbind(age,x))
+  currentID <- data_temp[i,1]
+  previousID <- data_temp[i-1,1]
+  currentstatus <- data_temp[i,4]
+  previousstatus <- data_temp[i-1,4]
+  if(currentID == previousID && currentstatus==1 && previousstatus==0){ #if(participant became diabetic)
+    diff<-data_temp[i,6] - data_temp[i-1,6]                             #diff = change in glucose level 
+    age <- data_temp[i, 2]                                              #age = age at which they test diabetic
+    data_diff<-rbind(data_diff,cbind(age,diff))                         #(?? Just use c for age and diff)
+  }
 }
-}
+colnames(data_diff)<-c("Ages","Diff")
+plot(data_diff$Ages,data_diff$Diff)
